125863c0bf2dc0238374781049cfdb75fa37f721
Chenrui <631807682@qq.com>
2021-06-07 15:31:41 +0800

    test: more test

--- export_test.go
+++ export_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"math"
+	"math/rand"
 	"reflect"
 	"testing"
 	"time"
@@ -132,50 +133,108 @@ func TestTimeTypeExportExcel(t *testing.T) {
 
 }
 
-// func TestExportExcel(t *testing.T) {
-// 	sheetName := "sheet1"
-// 	type Test struct {
-// 		CompletePercent float64 `json:"complete_percent" excel:"header(百分比);index(1);format(percent)"`
-// 		UserName        string  `json:"user_name" excel:"header(学员);index(0);default(---)"`
-// 	}
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// 	datas := make([]Test, 0)
-// 	datas = append(datas, Test{
-// 		UserName:        "A",
-// 		CompletePercent: 15.3,
-// 	}, Test{
-// 		UserName:        "B",
-// 		CompletePercent: 17.558,
-// 	})
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
 
-// 	file, err := ExportExcel(sheetName, datas)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+type sortExportModel struct {
+	StringA string `excel:"index(2)"`
+	StringB string `excel:"index(1)"`
+	StringC string `excel:"index(0)"`
+}
 
-// 	for rIndex, row := range file.Sheet[sheetName].Rows {
-// 		if rIndex == 0 {
-// 			if row.Cells[0].String() != "学员" || row.Cells[1].String() != "百分比" {
-// 				t.Fatal(fmt.Errorf("head not equal:%v", row.Cells))
-// 			}
-// 			continue
-// 		}
+func TestSortExportExcel(t *testing.T) {
+	sheetName := "sheet1"
 
-// 		rowData := datas[rIndex-1]
-// 		for cIndex, cell := range row.Cells {
-// 			if cIndex == 0 {
-// 				userName := rowData.UserName
-// 				if userName != cell.String() {
-// 					t.Fatal(fmt.Errorf("user_name not equal data:%s xlxs:%s",
-// 						userName, cell.String()))
-// 				}
-// 			} else if cIndex == 1 {
-// 				targetPercent := fmt.Sprintf("%.2f%%", rowData.CompletePercent)
-// 				if targetPercent != cell.String() {
-// 					t.Fatal(fmt.Errorf("complete_percent not equal data:%s xlxs:%s",
-// 						targetPercent, cell.String()))
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+	originData := make([]sortExportModel, 0)
+	originData = append(originData, sortExportModel{
+		StringA: randStringBytes(10),
+		StringB: randStringBytes(10),
+		StringC: randStringBytes(10),
+	}, sortExportModel{
+		StringA: randStringBytes(10),
+		StringB: randStringBytes(10),
+		StringC: randStringBytes(10),
+	})
+
+	file, err := ExportExcel(sheetName, originData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var xlsxBuf bytes.Buffer
+	err = file.Write(&xlsxBuf)
+	if err != nil {
+		return
+	}
+
+	xlsxBytes := xlsxBuf.Bytes()
+	targetDatas := make([]sortExportModel, 0)
+	err = ImportExcel(xlsxBytes, sheetName, &targetDatas)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(originData, targetDatas) {
+		t.Fatal(fmt.Errorf("origin target not equal \n origin:%+v \n target:%+v",
+			originData, targetDatas))
+	}
+}
+
+type formatExportOriginModel struct {
+	Percent float64 `excel:"index(0);format(percent)"`
+}
+
+type formatExportTargetModel struct {
+	PercentString string `excel:"index(0);"`
+}
+
+func TestFormatExportExcel(t *testing.T) {
+	sheetName := "sheet1"
+
+	originData := make([]formatExportOriginModel, 0)
+	originData = append(originData, formatExportOriginModel{
+		Percent: rand.Float64(),
+	}, formatExportOriginModel{
+		Percent: rand.Float64(),
+	})
+
+	file, err := ExportExcel(sheetName, originData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var xlsxBuf bytes.Buffer
+	err = file.Write(&xlsxBuf)
+	if err != nil {
+		return
+	}
+
+	xlsxBytes := xlsxBuf.Bytes()
+	targetDatas := make([]formatExportTargetModel, 0)
+	err = ImportExcel(xlsxBytes, sheetName, &targetDatas)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(originData) != len(targetDatas) {
+		t.Fatal(fmt.Errorf("origin target len not equal"))
+	}
+
+	for i := 0; i < len(originData); i++ {
+		oData := originData[i]
+		tData := targetDatas[i]
+
+		targetPercent := fmt.Sprintf("%.2f%%", oData.Percent)
+		if targetPercent != tData.PercentString {
+			t.Fatal(fmt.Errorf("complete_percent not equal data:%s xlxs:%s",
+				targetPercent, tData.PercentString))
+		}
+	}
+}
